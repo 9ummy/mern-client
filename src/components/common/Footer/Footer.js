7c6f204e2ea5cae6e032ef50bb20e2db9b159960
Juyoung <godante27@gmail.com>
2022-04-19 02:25:56 +0900

    feat: Add Footer component

---
+++ src/components/common/Footer/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <span>&copy; 9ummy 2022</span>
+    </footer>
+  );
+}
+
+export default Footer;
