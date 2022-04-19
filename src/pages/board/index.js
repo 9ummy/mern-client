e85a2abca492ce4cc9154e954ababc19af9383af
Juyoung <godante27@gmail.com>
2022-04-19 10:03:15 +0900

    feat: Add files for board pages

---
+++ src/pages/board/index.js
@@ -0,0 +1,9 @@
+function Board() {
+  return (
+    <>
+      <div>게시글 없음</div>
+    </>
+  );
+}
+
+export default Board;
---
+++ src/pages/board/new.js
@@ -0,0 +1,9 @@
+function NewPost() {
+  return (
+    <>
+      <div>게시글 작성</div>
+    </>
+  );
+}
+
+export default NewPost;
