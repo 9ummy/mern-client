538187a81a5d90fe36eecdb7195bdec26f49bbb2
Juyoung <godante27@gmail.com>
2022-04-20 11:08:48 +0900

    feat: Add function getting time from server

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 function Home() {
-  return <div>Home</div>;
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/now')
+      .then((res) => {
+        const data = res.data;
+        document.getElementById('timeZone').innerHTML = data.now;
+      })
+      .catch((e) => {
+        alert(`서버에 연결하는 중 에러가 발생했습니다.\n${e}`);
+      });
+  }, []);
+  return (
+    <>
+      <div>Home</div>
+      <div id='timeZone'></div>
+    </>
+  );
 }
 
 export default Home;
