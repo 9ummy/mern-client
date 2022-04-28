60bdb2119441aa3b0b83c74b4c051ddcb74c6d41
Juyoung <godante27@gmail.com>
2022-04-29 02:07:49 +0900

    feat: Add getGames to board

--- src/pages/board/index.js
+++ src/pages/board/index.js
@@ -1,8 +1,52 @@
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+
 function Board() {
+  const columns = ['순위', '게임 제목', '역대 최다 동시 접속자 수'];
+  const [data, setData] = useState([]);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/game/all')
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <>
-      <div>게시글 없음</div>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th colSpan={3}>
+            <h1>게임 순위</h1>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          {columns.map((column, idx) => (
+            <td key={idx}>{column}</td>
+          ))}
+        </tr>
+        {!data ? (
+          <tr>
+            <td colSpan={6}>데이터가 없습니다.</td>
+          </tr>
+        ) : (
+          data.map((game) => (
+            <tr key={game._id}>
+              <td>{game.rank}</td>
+              <td>{game.name}</td>
+              <td>{game.allTimePeak}</td>
+            </tr>
+          ))
+        )}
+      </tbody>
+    </table>
   );
 }
 
