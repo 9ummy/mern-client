6fb2583e2a3482ce69bac16d08ad7f83c1bc04de
Juyoung <godante27@gmail.com>
2022-04-29 03:06:11 +0900

    feat: Add board by id page

--- src/pages/board/[id].js
+++ src/pages/board/[id].js
@@ -1,8 +1,110 @@
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
+
 function Post() {
+  const [isModifying, setIsModifying] = useState(false);
+  const [data, setData] = useState();
+  const [inputs, setInputs] = useState({
+    rank: '',
+    name: '',
+    allTimePeak: '',
+  });
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/game/${id}`)
+      .then((res) => {
+        setData(res.data[0]);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
+
+  useEffect(() => {
+    if (data)
+      setInputs({
+        rank: data.rank,
+        name: data.name,
+        allTimePeak: data.allTimePeak,
+      });
+  }, [data]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:5000/game/${id}`, inputs)
+      .then((res) => {
+        setData({
+          ...data,
+          rank: inputs.rank,
+          name: inputs.name,
+          allTimePeak: inputs.allTimePeak,
+        });
+        setIsModifying(false);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <>
-      <div>Post</div>
-    </>
+    <div>
+      {data && (
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <h1>게임 정보</h1>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>순위</td>
+              <td>제목</td>
+              <td>역대 최다 동시 접속자 수</td>
+            </tr>
+            <tr>
+              <td>{data.rank}</td>
+              <td>{data.name}</td>
+              <td>{data.allTimePeak}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
+      {isModifying && data && (
+        <>
+          <input
+            name='rank'
+            value={inputs.rank}
+            type='number'
+            onChange={handleChange}
+          />
+          <input
+            name='name'
+            value={inputs.name}
+            type='text'
+            onChange={handleChange}
+          />
+          <input
+            name='allTimePeak'
+            value={inputs.allTimePeak}
+            type='number'
+            onChange={handleChange}
+          />
+          <button onClick={handleUpdate}>완료</button>
+        </>
+      )}
+      <button onClick={() => setIsModifying(true)}>수정하기</button>
+      <button>삭제하기</button>
+    </div>
   );
 }
 
