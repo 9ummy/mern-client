626b36f33d228d423ca565cdae0f43f162475abb
Juyoung <godante27@gmail.com>
2022-04-29 14:25:39 +0900

    feat: Add BoardRow component

---
+++ src/components/BoardRow/BoardRow.js
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './BoardRow.module.css';
+
+function BoardRow({ id, rank, imageUrl, name, allTimePeak }) {
+  console.log('board row', id, rank, imageUrl, name, allTimePeak);
+  return (
+    <Link href={`/board/${id}`} passHref>
+      <a className={styles.container}>
+        <div className={styles.rank}>{rank}</div>
+        {imageUrl ? (
+          <div className={styles.imageUrl}>
+            <Image src={imageUrl} alt={name} width={120} height={45} />
+          </div>
+        ) : (
+          <div className={styles.imageUrl}></div>
+        )}
+        <divs className={styles.name}>{name}</divs>
+        <div className={styles.allTimePeak}>{allTimePeak}</div>
+      </a>
+    </Link>
+  );
+}
+
+export default BoardRow;
